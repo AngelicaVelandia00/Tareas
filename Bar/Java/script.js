35eda1d14674e2a5978cf5d819c6d493a616a20a
AngelicaVelandia00 <angelicavelandia97@hotmail.com>
2021-03-06 23:29:31 -0500

    Recuperacion Evaluacion

---
+++ Bar/Java/script.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+/* if ( condicion) {
+
+    codigo a ejecutar si esa condiion se cumple
+} else {
+    ejecutar por defecto
+}
+*/
+
+if (){
+
+}
+
+
+swal("Oops!", "Lo siento No puesdes Entrar", "error");
